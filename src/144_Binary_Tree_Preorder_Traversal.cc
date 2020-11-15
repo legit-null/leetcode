efd53def7920e871c531ac884f1e1b0126007037
Lukas Feng <feng.yu.pu0330@gmail.com>
2020-11-16 00:00:10 +0800

    144

---
+++ src/144_Binary_Tree_Preorder_Traversal.cc
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root == nullptr) return res;
+        
+        stack<TreeNode*> s;
+        s.push(root);
+        
+        TreeNode* top;
+        while (!s.empty()) {
+            top = s.top();
+            s.pop();
+            
+            res.push_back(top->val);
+            
+            if (top->right != nullptr) s.push(top->right);
+            if (top->left != nullptr) s.push(top->left);
+        }
+        
+        return res;
+    }
+};
